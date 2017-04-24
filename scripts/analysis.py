a8e1015756ec11095a0847eef24f478ce473702b
tgittelmacher <tgittelmacher@gmail.com>
2017-04-24 00:38:55 -0400

    Some new data testing

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -6,23 +6,43 @@ import csv
 import mysql.connector
 import config
 from config import db, table_name
+import sys
 
+#configure stdout to file
+orig_stdout = sys.stdout
+f = open('../output/output.txt', 'w')
+sys.stdout = f
 
+#Connect DB and import data
 mydb = mysql.connector.connect(user=db['user'],
     password=db['password'],
     host=db['host'],
     db=db['db'])
 cursor = mydb.cursor()
 
-
 sql = "select * from " + table_name
-
 crimes = pd.read_sql(sql, mydb)
+
+#Truncate needless first 3 columns
 crimes = crimes.iloc[:, 3: ]
 print(crimes.head())
 
+#Index by Date
+crimes = crimes.set_index('Crime_Date')
+crimes.index = pd.to_datetime(crimes.index)
+crimes.sort_index(inplace=True)
+print(crimes.shape)
+print(crimes.head())
+types = crimes[['Primary_Type']]
+print(types.head())
+
+crime_count = pd.DataFrame(types.groupby('Primary_Type').size().sort_values(ascending=False).rename('count').reset_index())
+print(crime_count.head())
 
 
 
 
+#handle closings
+sys.stdout = orig_stdout
+f.close()
 mydb.close()
